603d96cf93bde604aea614c400a706692ebeffc7
johnale <johnale@cisco.com>
2018-12-17 08:44:45 -0500

    functionality fix for clearing the game and new word after a win or a loss

--- assets/js/game-copy.js
+++ assets/js/game-copy.js
@@ -3,14 +3,13 @@
 // variables
 
 let lettersArray = [];
-let gameWords = ["thor", "hulk", "antman", "thanos", "drstrange", "ebonyma", "blackwidow", "blackpanther", "captainamerica", "captainmarvel", "spiderman", "ironman"];
-// let wordIndex = Math.floor(Math.random() * gameWords.length);
-let wordIndex, selectedWord, wordLength, display, win, letters;
-// let selectedWord = gameWords[wordIndex];
-// let wordLength = selectedWord.length;
-// let display = [wordLength];
-// let win = wordLength;
-// let letters = selectedWord.split('');
+let gameWords = ["thor", "hulk", "antman", "thanos", "drstrange", "ebonyma", "blackwidow", "blackpanther", "captainamerica", "captianmarvel", "spiderman", "ironman"];
+let wordIndex = Math.floor(Math.random() * gameWords.length);
+let selectedWord = gameWords[wordIndex];
+let wordLength = selectedWord.length;
+let display = [wordLength];
+let win = wordLength;
+let letters = selectedWord.split('');
 let remainingGuess = 10;
 let output = "";
 let usedLetter = "";
@@ -23,37 +22,18 @@ let flag;
 // add event listener
 window.addEventListener('keypress', onKeyPress);
 
-function ranWord(arr) {
-    wordIndex =  Math.floor(Math.random() * arr.length);
-    selectedWord = gameWords[wordIndex];
-    letters = selectedWord.split("");
-    display = [wordLength];
-    win = wordLength;
-    letters = selectedWord.split('');
-
-    for (var i=0; i < selectedWord.length; i++) {
-        display[i] = "_ ";
-        output = output + display[i];
-    }
-    document.getElementById("wordLayout").innerHTML = output;
-    //clear output
-    output = "";
-
-}
-
 
 // functions
 function onKeyPress(event) {
+    // if(event.keyCode >= 65 && event.keyCode <= 90 || event.keyCode >= 97 && event.keyCode <= 122 ) {
+    //     lettersArray.push(event.key);
+    //     document.getElementById("myArray").innerHTML = lettersArray;
+    // }
     if (lettersArray.includes(event.key)) {
         console.log("its in there already");
-    } else if (letters.includes(event.key)) {
-        lettersArray.push(event.key);
-        document.getElementById("myArray").innerHTML = lettersArray;
     } else {
         lettersArray.push(event.key);
         document.getElementById("myArray").innerHTML = lettersArray;
-        remainingGuess--;
-        document.getElementById("guessCount").innerHTML = remainingGuess;
     }
     gameLogic();
 
@@ -63,12 +43,6 @@ function onKeyPress(event) {
 function startGame() {
     remainingGuess = 10;
     lettersArray = [];
-    wordIndex =  Math.floor(Math.random() * gameWords.length);
-    selectedWord = gameWords[wordIndex];
-    letters = selectedWord.split("");
-    display = [wordLength];
-    win = wordLength;
-    letters = selectedWord.split('');
 
     for (var i=0; i < selectedWord.length; i++) {
         display[i] = "_ ";
@@ -78,9 +52,6 @@ function startGame() {
     //clear output
     output = "";
     document.getElementById("guessCount").innerHTML = remainingGuess;
-
-    document.getElementById("myArray").innerHTML = '';
-    // ranWord(gameWords);
 
 }
 
@@ -107,30 +78,55 @@ function gameLogic () {
     output = "";
     usedLetter = lettersArray;
 
+    // for (var x = 0; x < selectedWord.length; x++) {
+    //     if (usedLetter[x] == letters[x]) {
+    //         // display[x] = usedLetter[x];
+    //         win--;
+    //     }
+    
+    //     output = output + display[x] + " ";
+    // }
+    // document.getElementById("wordLayout").innerHTML = output;
+    // output = "";
+    // remainingGuess--;
+
     for (let i of getIndicies(usedLetter, letters)) {
         display[i] = letters[i] + " ";
-
-        
     }
     document.getElementById("wordLayout").innerHTML = display.join(" ");
     output = "";
+    remainingGuess--;
+    document.getElementById("guessCount").innerHTML = remainingGuess;
     ifWin();
+    
+
+
+    // determine a winner!
+
+    // if (win < 1) {
+    //     document.getElementById("winCount").innerHTML = winCount++;
+    //     // startGame();
+    // } else if (remainingGuess < 1) {
+    //     document.getElementById("messageBanner").innerHTML = "You Lose!!!!";
+    //     // startGame()
+    // } else {
+    //     document.getElementById("guessCount").innerHTML = remainingGuess;
+    // }
+
 }
 
 function ifWin() {
     if (document.getElementById("wordLayout").innerHTML.split(" ").join("") === letters.join("") && remainingGuess >= 1) {
+        console.log(document.getElementById("wordLayout").innerHTML)
         winCount = winCount + 1;
         document.getElementById("winCount").innerHTML = winCount;
-        startGame();
-        
+        reset();
     } else if (remainingGuess === 0) {
         document.getElementById("messageBanner").innerHTML = "You Lose!!!!";
         flag = confirm("Would you like to play again?");
         if (flag) {
             startGame();
             document.getElementById("messageBanner").innerHTML = "";
-            // ranWord(gameWords);
-            // reset();
         }
     }
 }
@@ -141,10 +137,8 @@ function reset() {
     remainingGuess = 10;
     document.getElementById("guessCount").innerHTML = remainingGuess;
     startGame();
-    // ranWord(gameWords);
 
 }
-
 
 
 
